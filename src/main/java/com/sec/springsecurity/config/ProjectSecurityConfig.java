dde091e5c58b71c0d79af819841b837f189ccd9a
Sanjeeb <sanjeeb116@gmail.com>
2025-10-03 07:07:36 +0530

    remove bcrypt prifix while register new user

--- src/main/java/com/sec/springsecurity/config/ProjectSecurityConfig.java
+++ src/main/java/com/sec/springsecurity/config/ProjectSecurityConfig.java
@@ -1,17 +1,12 @@
 package com.sec.springsecurity.config;
 
-import javax.sql.DataSource;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import com.sec.springsecurity.service.EasyBankUserDetailsService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -19,27 +14,38 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ProjectSecurityConfig {
 
-	private final EasyBankUserDetailsService easyBankUserDetailsService;
-    private final DataSource dataSource;
 	@Bean
 	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 
 		http.csrf(csrfConfig -> csrfConfig.disable())
 				.authorizeHttpRequests((requests) -> requests
 						.requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
-						.requestMatchers("/notices", "/contact", "/error", "/register","/css/**", "/js/**", "/images/**", "/favicon.ico").permitAll())
+						.requestMatchers("/notices", "/contact", "/error", "/register", "/css/**", "/js/**",
+								"/images/**", "/favicon.ico")
+						.permitAll())
 
 				.formLogin(Customizer.withDefaults()) // browser login form
 				.httpBasic(Customizer.withDefaults()); // API basic auth;
 
 		return http.build();
 	}
-	
+
+	// The {bcrypt} part is not random text—it’s a prefix added by
+	// DelegatingPasswordEncoder to indicate which algorithm was used for hashing.
+
+//	@Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//    }
+
+	// The {bcrypt} prefix is automatically added by Spring Security’s
+	// DelegatingPasswordEncoder to indicate which algorithm was used.
+	// If you use new BCryptPasswordEncoder(), the prefix won’t be added.
 	@Bean
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
-	
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(); // No {bcrypt} prefix
+	}
+
 	/*
 	 * @Bean public AuthenticationManager authManager(HttpSecurity http) throws
 	 * Exception { AuthenticationManagerBuilder authBuilder =
@@ -55,11 +61,11 @@ public class ProjectSecurityConfig {
 	 * 
 	 * return authBuilder.build(); }
 	 */
-	
+
 	public static void main(String[] args) {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		String hash = encoder.encode("EazyBytes@12345");
-		
+
 		System.out.println(hash);
 	}
 }
